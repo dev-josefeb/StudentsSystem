d8d6e411a739f80be41c917f58d0da76b28c0db8
dev-josefeb <febinjose@protonmail.com>
2023-04-30 11:14:25 +0200

    Add Validator for the UpdateStudentRequest

---
+++ API/Validators/UpdateStudentRequestValidator.cs
@@ -0,0 +1,30 @@
+﻿using API.Domain_Models;
+using API.Repositories;
+using FluentValidation;
+
+namespace API.Validators
+{
+    public class UpdateStudentRequestValidator : AbstractValidator<UpdateStudentRequest>
+    {
+        public UpdateStudentRequestValidator(IStudentRepository studentRepository)
+        {
+            RuleFor(x => x.FirstName).NotEmpty();
+            RuleFor(x => x.LastName).NotEmpty();
+            RuleFor(x => x.DateOfBirth).NotEmpty();
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+            RuleFor(x => x.Mobile).NotEmpty();
+
+            RuleFor(x => x.GenderId).NotEmpty()
+            .Must(id =>
+            {
+                var gender = studentRepository.GetGendersAsync().Result.ToList().FirstOrDefault(x => x.Id == id);
+                return gender != null;
+            })
+            .WithMessage("Please select a valid gender");
+
+            RuleFor(x => x.PhysicalAddress).NotEmpty();
+            RuleFor(x => x.PostalAddress).NotEmpty();
+        }
+    }
+}
+
